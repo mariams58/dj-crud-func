9583b8311a2925217034b45d1afe724af6489b3b
mariams58 <mariams58@yahoo.com>
2022-07-05 02:56:36 +0100

    First Commit

---
+++ I4G0172181XT/blog/views.py
@@ -0,0 +1,31 @@
+from calendar import c
+from dataclasses import fields
+from django.shortcuts import render
+from django.urls import reverse_lazy
+
+# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = PostCreateView
+    
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDetailView (DetailView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class  PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")    
